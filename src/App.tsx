03d0c0579a19f9ab2476a16f2f7bc7d164ee2df3
TanyaDovzhenko <tanyadovzzhenko@gmail.com>
2022-10-27 17:16:00 +0300

    husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
 import './Global.scss'
 
 function App() {
-  // console
-  //   .log(
-  //     113
-  //     +
-  //     'sdfsd')
+  console.log(123)
   return (
     <div className='app'>
-      <Test prop={false} />
     </div>
   );
 }
 
 export default App;
 
-interface test {
-  prop: boolean
-}
-
-function Test({ prop }: test) {
-  console
-    .log('sdfsd')
-  return (
-    <div className='app'>
-    </div>
-  );
-}
